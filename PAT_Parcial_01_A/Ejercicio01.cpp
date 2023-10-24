766a52db78737934bc6bc58a208e379ea5cdf7bc
MauricioECabrera <123261296+MauricioECabrera@users.noreply.github.com>
2023-10-24 08:29:26 -0600

    Ejercicio01.cpp

--- PAT_Parcial_01_A/Ejercicio01.cpp
+++ PAT_Parcial_01_A/Ejercicio01.cpp
@@ -1,6 +1,23 @@
 #include "Ejercicio01.h"
 
-Node<char>* Ejercicio01::remove(Node<char>* head)
-{
-    return nullptr;
-}
+Node<char>* Ejercicio01::remove(Node<char>* head) {
+    if (head == nullptr) {
+        return nullptr; 
+    }
+
+    Node<char>* current = head;
+
+    for (current->next != nullptr) {
+        if (current->value == current->next->value) {
+  
+            Node<char>* temp = current->next;
+            current->next = temp->next;
+            delete temp;
+        }
+        else {
+            current = current->next;
+        }
+    }
+
+    return head;
+};
