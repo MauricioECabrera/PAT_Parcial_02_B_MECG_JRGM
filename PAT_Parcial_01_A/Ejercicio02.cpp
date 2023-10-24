3c6101808eb6b4a7ccee48cd921d50ccded412df
MauricioECabrera <123261296+MauricioECabrera@users.noreply.github.com>
2023-10-24 08:24:34 -0600

    Ejercicio02.cpp

--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -2,5 +2,28 @@
 
 int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
 {
-    return 0;
+    int studentIndex = 0;
+    int sandwichIndex = 0;
+    int unableToEat = 0
+
+        while (studentIndex < students->size() && sandwichIndex < sandwiches->size()) {
+            if ((*students)[studentIndex] == (*sandwiches)[sandwichIndex]) {
+
+                studentIndex++;
+            }
+
+            sandwichIndex++;
+
+            if (studentIndex == students->size()) {
+
+                break;
+            }
+            if (sandwichIndex == sandwiches->size()) {
+
+                unableToEat = students->size() - studentIndex;
+                break;
+            }
+        }
+
+    return unableToEat;
 }
